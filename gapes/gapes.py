2518e92117e450bf3f28d931187cdb3ca10d4017
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2022-10-08 16:33:02 +0900

    make window showing

--- gapes/gapes.py
+++ gapes/gapes.py
@@ -10,6 +10,11 @@ clock = pygame.time.Clock()
 scene_manager = manager_scene.SceneManager()
 
 
+def init(window_size=(1200, 800), caption=""):
+    pygame.display.set_mode((window_size[0], window_size[1]))
+    pygame.display.set_caption(caption)
+
+
 def run(fps=60):
     gameloop = True
     while gameloop:
